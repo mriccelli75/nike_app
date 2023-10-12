e9cc37a7c0adf92cda73c39c94e7a388133d5aff
Mike Riccelli <mriccelli@ons.org>
2023-10-12 09:36:26 -0400

    more component structure and content

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,34 @@ import {
   Subscribe,
   SuperQuality
 } from "./sections";
+import Nav from "./components/Nav";
 
 const App = () => (
  <main className="relative">
+  <Nav />
   <section className="xl:padding-1 wide:padding-r padding-b">
-    Hero
+    <Hero />
   </section>
   <section className="padding">
-    PopularProducts
+    <PopularProducts />
   </section>
   <section className="padding">
-    SuperQuality
+    <SuperQuality />
   </section>
   <section className="padding-x py-10">
-    Services
+    <Services />
   </section>
   <section className="padding">
-    SpecialOffer
+    <SpecialOffers />
   </section>
   <section className="bg-pale-blue padding">
-    CustomerReviews
+    <CustomerReviews />
   </section>
   <section className="padding-x sm:py-32 py-16 w-full">
-    Subscribe
+    <Subscribe />
   </section>
   <section className="bg-black padding-x padding-t pb-8">
-    Footer
+    <Footer />
   </section>
  </main>
 );
